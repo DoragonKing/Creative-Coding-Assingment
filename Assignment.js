d46a428579fa0c6e45ce9e41cfa953899fee976d
DoragonKing <Saskueuzumaki@gmail.com>
2018-11-15 21:37:27 +0000

    now changed to an array instead of button presses

--- Assignment.js
+++ Assignment.js
@@ -1,36 +1,34 @@
 var click=0;
 let posx;
 let posy;
-var colour;
+let r1;
+let g2;
+let b2;
+
+var r=[255,30,150,100,0];
+var g=[0,200,150,100,0];
+var b=[0,0,255,255,0];
+var index=0
+
 function setup () {
   strokeWeight(1);
   createCanvas(1000,1000);
-  frameRate(100)
+  frameRate(100);
 background(0);
-for(let x=0;x<10;x=x+1){
-  fill(random(255),random(255),random(255))
-  posx=random(width);
-  posy=random(height);
-
-  rect(posx,posy,100,100);}
 }
 
 function draw () {
-  if(colour==0){
-  fill(0,255,0)
-
-}
-else{
-  fill(random(255),random(255),random(255))
-}
-  if(colour==1){
-    fill(255,0,0)
-  }
-
+fill(r[index],g[index],b[index]);
 noStroke();
+if(index==4){
+ r1=random(255);
+ g2=random(255);
+ b2=random(255);
+  fill(r1,g2,b2);
+}
 
   if(click==1){
-    rect(mouseX,mouseY,30,30)
+    ellipse(mouseX,mouseY,30,30)
   }
 
 }
@@ -46,9 +44,8 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-  if(key=='1')colour=0
-  if(key=='2')colour=1
-  if(key=='0')colour=100
+index=index+1
+if(index>4){index=0}
 }
 
 //for the loop criteria instead of only having 1 shape everytime you click, have a line of shapes.
