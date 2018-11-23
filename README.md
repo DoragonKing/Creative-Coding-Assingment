9129a93b4f6a78e59139b95e40c1fdefe061c044
DoragonKing <Saskueuzumaki@gmail.com>
2018-11-23 15:53:35 +0000

    Noise I believe this has the addittion of the perline noise for the rainbow so its less sudden and flows more, I like the look of it

---
+++ README.md
@@ -0,0 +1,6 @@
+# Creative Coding Assingment
+
+The code creates a canvas of which the user can interact with it, painting something they want.
+They can scroll the mouse wheel to change the size of the circles drawn. To change the colour
+simply press any key on the keyboard and it will change to the next colour in the array.
+Clicking draws the circle, when you release the mouse it will stop drawing.
--- assignment/Assignment.js
+++ assignment/Assignment.js
@@ -7,23 +7,42 @@ let g2;
 let b2;
 let size=30;
 var colour={
- r:[255,30,150,255,0],
- g:[0,200,150,0,0],
- b:[0,0,255,255,0]};
- var cname=['Colour1','Colour2','Colour3','Colour4','Rainbow']
-var index=0
+ r:[255,30,150,255,0,0],
+ g:[0,200,150,0,0,0],
+ b:[0,0,255,255,0,0]};
+ var cname=['Colour1','Colour2','Colour3','Colour4','Rainbow','Black/Erase']
+var index=0;
+var xoff=0;
+//var s;
+var yoff1=0;
+var yoff2=200;
+var yoff3=600;
+var col1;
+var col2;
+var col3;
+
+
 
 function setup () {
   strokeWeight(1);
   createCanvas(600,600);
   frameRate(100);
 background(0);
-
 }
 
 function draw () {
+  var col1= map(noise(yoff1),0,1,0,255);
+  var col2= map(noise(yoff2),0,1,0,255);
+  var col3= map(noise(yoff3),0,1,0,255);
+//  var s= map(noise(xoff),0,1,0,60);
+  var rainbow={
+    r:col1,
+    g:col2,
+    b:col3,
+  }
+
 fill(0)
-rect(0,0,160,40)
+rect(0,0,220,40)
 //Add text stating what the colour is so uses know what the colour is
   strokeWeight(1);
   textSize(30);
@@ -34,28 +53,28 @@ fill(colour.r[index],colour.g[index],colour.b[index]);
 noStroke();
 //The random colour, so every circle colour is random
 if(index==4){
- r2=random(255);
- g2=random(255);
- b2=random(255);
- fill(r2,g2,b2);
+ //r2=random(255);
+ //g2=random(255);
+ //b2=random(255);
+ fill(rainbow.r,rainbow.g,rainbow.b);
 }
 
   if(click==1){
-    ellipse(mouseX,mouseY,size,size)
+    ellipse(mouseX,mouseY,size)
+  //  xoff+=0.01
+    yoff1+=0.02
+    yoff2+=0.02
+    yoff3+=0.02
   }
-
-
 }
-
 /*This is so I can activate when to start drawing which is when
 the mouse is clicked and stop drawing when the mouse is released*/
-
 function mousePressed(){
-  click=click+1
+  // changed from adding 1 to click to making it one, found a bug if you somehow click while the mouse is held and vice versa for the release
+  click=1
 }
-
 function mouseReleased(){
-  click=click-1
+  click=0
 }
 /*The code here changes the colour, I have it so if any key is press the colour changes
 and made sure that the colour doesn't change whenever I press 0 to clear the canvas*/
@@ -66,6 +85,9 @@ index=index+1
 if(index==colour.r.length){index=0}
 }
 }
+
+
+
 //This is the code that adjusts the size of the circles
 function mouseWheel(event){
   var movement=event.delta
@@ -73,8 +95,8 @@ function mouseWheel(event){
 if(movement<-1){movement=movement+115}
 if(movement>+1){movement=movement-115}
 size=movement+size
-/*The console log is just so I can see if the size is changing as it should be
-and to figure out how much the mousewheel changed the size*/
+//The console log is just so I can see if the size is changing as it should be
+//and to figure out how much the mousewheel changed the size
   console.log(size)
   //Just adds a minimum size so that it doesn't get ridicolously small
   if(size<15){
@@ -84,4 +106,6 @@ and to figure out how much the mousewheel changed the size*/
 }
 
 //idea,if the there's a large amount of circles on the screen have all the balls drop or have something happen.
-//use the perlin noise to draw instead of having the person draw 
+//use the perlin noise to draw instead of having the person draw
+//better idea, use the perlin noise for the rainbow instead of random
+//and use it to change the size of the brush
